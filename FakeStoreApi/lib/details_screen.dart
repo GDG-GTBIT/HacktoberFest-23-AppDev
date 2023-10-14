fa3312a32760076280b02011b3128f446ddf8fd4
Gagandeep <gagandeepkaur1523@gmail.com>
2023-10-14 14:25:03 +0530

    Added product details screen

---
+++ FakeStoreApi/lib/details_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  String imgUrl;
+  String title;
+  double price;
+  String desc;
+  DetailsScreen(
+      {super.key,
+      required this.imgUrl,
+      required this.title,
+      required this.price,
+      required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Container(
+                height: 130,
+                width: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(imgUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(title),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(desc),
+            const SizedBox(
+              height: 20,
+            ),
+            Text('₹${price.toStringAsFixed(2)}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
